857c746901862092b687b832e8186bbf7978d0f5
Behr <67525394+alexbehrens@users.noreply.github.com>
2025-05-28 15:22:04 -0700

    Fix COEP headers configuration

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,8 @@ export default defineConfig({
   server: {
     headers: {
       'Cross-Origin-Opener-Policy': 'same-origin',
-      'Cross-Origin-Embedder-Policy': 'require-corp'
+      'Cross-Origin-Embedder-Policy': 'require-corp',
+      'Cross-Origin-Resource-Policy': 'cross-origin'
     }
   },
   build: {
